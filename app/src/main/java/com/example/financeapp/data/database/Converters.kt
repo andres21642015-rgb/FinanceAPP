b67a4630742b56f0660818a7861e0eb90412e224
andres21642015-rgb <andres21642015@gmail.com>
2026-04-30 22:26:02 -0500

    feat(database): created db, file  AppDatabase and Converts in feature/data-database

---
+++ app/src/main/java/com/example/financeapp/data/database/Converters.kt
@@ -0,0 +1,18 @@
+package com.example.financeapp.data.database
+
+import androidx.room.TypeConverters
+import com.example.financeapp.data.model.ExpenseCategory
+
+//conversion de expensecategory a string
+class Converters {
+
+    @TypeConverters
+    fun fromExpenseCategory(category: ExpenseCategory): String {
+        return category.name
+    }
+
+    @TypeConverters
+    fun toExpenseCategory(value: String) : ExpenseCategory{
+        return ExpenseCategory.valueOf(value)
+    }
+}
